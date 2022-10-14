0501a45c9b6d97931308c94e4ad9f36249401b66
Dan <105542989+4aniDev@users.noreply.github.com>
2022-10-14 15:43:39 +0500

    Card and Box with color

--- app/src/main/java/ru/chani/composetraining/MainActivity.kt
+++ app/src/main/java/ru/chani/composetraining/MainActivity.kt
@@ -3,26 +3,88 @@ package ru.chani.composetraining
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.ScrollableState
 import androidx.compose.foundation.gestures.rememberScrollableState
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Column {
+                listItem(name = "Yarty", message = "Kim gullak?")
+                listItem(name = "Yarty", message = "EEEEE ejem....")
+                listItem(name = "Yarty", message = "Mana diya?")
+            }
+        }
+    }
+}
 
+
+
+@Composable
+private fun listItem(name: String, message: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+            .background(Color.Blue),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(
+            modifier = Modifier
+                .background(Color.Green)
+        ) {
+            Row(
+                modifier = Modifier
+                    .background(Color.DarkGray),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.yarty),
+                    contentDescription = "yarty gullak",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(60.dp)
+                        .clip(CircleShape)
+                )
+                Column(
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .fillMaxWidth()
+                        .background(Color.Red),
+                    verticalArrangement = Arrangement.Center,
+                ) {
+                    Text(
+                        text = name,
+                        modifier = Modifier
+                            .background(Color.Yellow)
+                    )
+                    Text(text = message)
+                }
+            }
         }
     }
 }
